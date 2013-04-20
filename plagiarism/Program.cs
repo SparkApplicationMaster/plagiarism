be8f840bd14370524531b07d3df461815172e210
Евгений <glot.unltd@gmail.com>
2013-04-20 16:43:15 +0400

    забыл создать директории

--- plagiarism/Program.cs
+++ plagiarism/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,8 @@ namespace plagiarism
         [STAThread]
         static void Main()
         {
+            Directory.CreateDirectory("./programfiles");
+            Directory.CreateDirectory("./userfiles");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
