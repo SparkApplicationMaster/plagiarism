c4610e8754d6189d5cf8a75fed8a4668939cfcac
Евгений <glot.unltd@gmail.com>
2013-04-23 23:36:25 +0400

    Значительно доработан алгоритм
    Убрано всё тупое,теперь вместо квадрата за линию работает

--- plagiarism/shingles.cs
+++ plagiarism/shingles.cs
@@ -1,19 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using plagiarism;
 
 namespace dataAnalyze.Algorithms
 {
     public class Shingles
     {
-        public static bool Checkinputfile = true;
+        static public bool Checkinputfile = true;
         private char[] _stopSymbols;
         public int ShingleLength;
 
         public Shingles(string delims, int shinglelength)
         {
-//             if (!File.Exists(delims))
-//                 throw new Exception("File with stop symbols not exists: " + delims);
             _stopSymbols = delims.ToCharArray();
             ShingleLength = shinglelength;
 
@@ -27,89 +27,74 @@ namespace dataAnalyze.Algorithms
             return ShingleLength;
         }
 
-        public double CompareStrings(string s1, string s2)
+        public double CompareStrings(string s1, string s2, int length)
         {
-            if (s1.Length <= ShingleLength) return 0.0;
-            if (s2.Length <= ShingleLength) return 0.0;
-            if (s1.Length > s2.Length)
-            {
-                if (Math.Abs((s1.Length / (double)s2.Length)) >= 1.7)
-                    return 0.0;
-            }
-            else
-            {
-                if (Math.Abs((s2.Length / (double)s2.Length)) >= 1.7)
-                    return 0.0;
-            }
-
-
+            ShingleLength = length;
             RemoveStopSymbols(ref s1);
             RemoveStopSymbols(ref s2);
-
-            if (s1.Length <= ShingleLength) return 0.0;
-            if (s2.Length <= ShingleLength) return 0.0;
-            var shingles1 = GetShingles(ref s1, ShingleLength);
-            var shingles2 = GetShingles(ref s2, ShingleLength);
+            HashSet<string> shingles1, shingles2;
             if (Checkinputfile)
             {
-                var same = shingles1.Count(shingles2.Contains);
-                return same/((double) (shingles1.Length))*100;
+                shingles1 = GetShingles(ref s1, ShingleLength);
+                shingles2 = GetShingles(ref s2, ShingleLength);
             }
             else
             {
-                var same = shingles2.Count(shingles1.Contains);
-                return same / ((double)(shingles2.Length)) * 100;
+                shingles1 = GetShingles(ref s2, ShingleLength);
+                shingles2 = GetShingles(ref s1, ShingleLength);
             }
+            var same = shingles1.Count(shingles2.Contains);
+            return same /((double) (shingles1.Count()))*100;
         }
 
 
 
 
 
-        public double CompareStringsCashed(ref string[] shingles1, ref string[] shingles2, string s1, string s2)
-        {
-            if (s1 != null && s2 != null)
-            {
-                if (s1.Length > s2.Length)
-                {
-                    if (Math.Abs((s1.Length / (double)s2.Length)) >= 1.7)
-                        return 0.0;
-                }
-                else
-                {
-                    if (Math.Abs((s2.Length / (double)s1.Length)) >= 1.7)
-                        return 0.0;
-                }
-            }
-
-            if (s1 != null)
-            {
-                if (s1.Length <= ShingleLength) return 0.0;
-                string inS1 = s1;
-                RemoveStopSymbols(ref inS1);
-                if (inS1.Length <= ShingleLength) return 0.0;
-                shingles1 = GetShingles(ref inS1, ShingleLength);
-            }
-
-            if (s2 != null)
-            {
-                if (s2.Length <= ShingleLength) return 0.0;
-                var inS2 = s2;
-                RemoveStopSymbols(ref inS2);
-                if (inS2.Length <= ShingleLength) return 0.0;
-                shingles2 = GetShingles(ref inS2, ShingleLength);
-            }                        
-           
-            var same = 0;
-            foreach (var t in shingles1)
-            {
-                if (shingles2.Contains(t))
-                    same++;
-            }
-
-            return same * 2 / ((double)(/*shingles1.Length + */shingles2.Length)) * 100;
-          
-        }
+//         public double CompareStringsCashed(ref string[] shingles1, ref string[] shingles2, string s1, string s2)
+//         {
+//             if (s1 != null && s2 != null)
+//             {
+//                 if (s1.Length > s2.Length)
+//                 {
+//                     if (Math.Abs((s1.Length / (double)s2.Length)) >= 1.7)
+//                         return 0.0;
+//                 }
+//                 else
+//                 {
+//                     if (Math.Abs((s2.Length / (double)s1.Length)) >= 1.7)
+//                         return 0.0;
+//                 }
+//             }
+// 
+//             if (s1 != null)
+//             {
+//                 if (s1.Length <= ShingleLength) return 0.0;
+//                 string inS1 = s1;
+//                 RemoveStopSymbols(ref inS1);
+//                 if (inS1.Length <= ShingleLength) return 0.0;
+//                 shingles1 = GetShingles(ref inS1, ShingleLength);
+//             }
+// 
+//             if (s2 != null)
+//             {
+//                 if (s2.Length <= ShingleLength) return 0.0;
+//                 var inS2 = s2;
+//                 RemoveStopSymbols(ref inS2);
+//                 if (inS2.Length <= ShingleLength) return 0.0;
+//                 shingles2 = GetShingles(ref inS2, ShingleLength);
+//             }                        
+//            
+//             var same = 0;
+//             foreach (var t in shingles1)
+//             {
+//                 if (shingles2.Contains(t))
+//                     same++;
+//             }
+// 
+//             return same * 2 / ((double)(/*shingles1.Length + */shingles2.Length)) * 100;
+//           
+//         }
 
 
         /// <summary>
@@ -118,17 +103,33 @@ namespace dataAnalyze.Algorithms
         /// <param name="source"></param>
         /// <param name="shingleLength"></param>
         /// <returns></returns>
-        private string[] GetShingles(ref string source, int shingleLength)
+//         private int[] GetShingles(ref string source, int shingleLength)
+//         {
+//             var shingles = new int[source.Length - (shingleLength - 1)];
+//             var shift = 0;
+//             for (var i = 0; i < shingles.Length; i++)
+//             {
+// 
+//                 shingles[i] = 
+//                     (source.Length >= shift + shingleLength)
+//                         ? source.Substring(shift, shingleLength).GetHashCode()
+//                         : source.Substring(shift, source.Length - (shift + shingleLength)).GetHashCode();
+//                 shift++;
+//             }
+//             return shingles;
+// 
+//         }
+        private HashSet<string> GetShingles(ref string source, int shingleLength)
         {
-            var shingles = new string[source.Length - (shingleLength - 1)];
+            var shingles =new HashSet<string>(); //new string[source.Length - (shingleLength - 1)];
             var shift = 0;
-            for (var i = 0; i < shingles.Length; i++)
+            for (var i = 0; i < source.Length - (shingleLength - 1); i++)
             {
 
-                shingles[i] += 
+                shingles.Add(
                     (source.Length >= shift + shingleLength)
-                        ? source.Substring(shift, shingleLength).GetHashCode()
-                        : source.Substring(shift, source.Length - (shift + shingleLength)).GetHashCode();
+                        ? source.Substring(shift, shingleLength)
+                        : source.Substring(shift, source.Length - (shift + shingleLength)));
                 shift++;
             }
             return shingles;
